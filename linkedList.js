4627274fda0ac873d1c238deac4f9ff5bb45b88a
Ayesh Biswal <biswalayesh3@gmail.com>
2022-09-02 21:57:21 +0530

    Implement head and pop methods

--- linkedList.js
+++ linkedList.js
@@ -6,15 +6,15 @@ const node = (data = null) => {
 };
 
 const linkedList = () => {
-  head = null;
+  first = null;
 
   // Appends a node to the end of the list.
   const append = value => {
     const newNode = node(value);
 
-    if (head === null) head = newNode;
+    if (first === null) first = newNode;
     else {
-      let temp = head;
+      let temp = first;
       while (temp.next !== null) temp = temp.next;
 
       temp.next = newNode;
@@ -25,13 +25,13 @@ const linkedList = () => {
   const prepend = value => {
     const newNode = node(value);
 
-    newNode.next = head;
-    head = newNode;
+    newNode.next = first;
+    first = newNode;
   };
 
   // Prints the list.
   const toString = () => {
-    let temp = head;
+    let temp = first;
     let str = '';
     while (temp !== null) {
       str += temp.value() + ' -> ';
@@ -43,7 +43,7 @@ const linkedList = () => {
 
   // Returns the total number of nodes in the list.
   const size = () => {
-    let temp = head;
+    let temp = first;
     count = 0;
     while (temp !== null) {
       count++;
@@ -53,11 +53,26 @@ const linkedList = () => {
     return count;
   };
 
+  // Returns the first node of the list.
+  const head = () => first.value();
+
   // Returns the last node of the list.
   const tail = () => {
-    let temp = head;
+    let temp = first;
     while (temp.next !== null) temp = temp.next;
     return temp.value();
+  };
+
+  // Pops the last node from the list.
+  const pop = () => {
+    let temp = first;
+    for (let i = 0; i < size() - 2; i++) {
+      temp = temp.next;
+    }
+    const popped = temp.next.value();
+    delete temp.next;
+    temp.next = null;
+    return popped;
   };
 
   return {
@@ -65,6 +80,21 @@ const linkedList = () => {
     toString,
     prepend,
     size,
+    head,
     tail,
+    pop,
   };
 };
+
+const list = linkedList();
+list.append(400);
+list.append(500);
+list.prepend(300);
+list.prepend(200);
+list.prepend(100);
+console.log(list.toString());
+console.log(list.size());
+console.log(list.tail());
+console.log(list.head());
+console.log('Popped', list.pop());
+console.log(list.toString());
